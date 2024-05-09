1e94c6024c0aba7342cfac4386ae7e539fb3ed12
digocoutinho78 <digocoutinho@outlook.com>
2024-05-09 16:44:15 -0300

    icones botton tabs

--- Projeto-Ecommerce/src/routes/bottom-tabs-routes.js
+++ Projeto-Ecommerce/src/routes/bottom-tabs-routes.js
@@ -1,16 +1,57 @@
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
-import TabUm from '../screens/TabUm' 
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import TabUm from "../screens/TabUm";
+import { FontAwesome } from "@expo/vector-icons";
 
-const { Navigator, Screen} = createBottomTabNavigator();
+const { Navigator, Screen } = createBottomTabNavigator();
 
 export default Tabs = () => {
+  return (
+    <Navigator
+      screenOptions={{
+        // esconder o header
+        headerShown: false,
+      }}
+    >
 
-  return(
-    <Navigator screenOptions={{
-      // esconder o header
-      headerShown: false,
-    }}> 
-      <Screen name='tela1' component={TabUm}/>
+
+
+      <Screen name="Home"
+        component={TabUm}
+        options={{
+          headerShown: false,
+          // title: "Início",
+          tabBarIcon: ({ size, color }) => (
+            <FontAwesome name="home" color={color} size={size} />
+          ),
+        }} />
+
+      <Screen name="Tela 1"
+        component={TabUm}
+        options={{
+          headerShown: false,
+          // title: "Início",
+          tabBarIcon: ({ size, color }) => (
+            <FontAwesome name="tag" color={color} size={size} />
+          ),
+        }} />
+        <Screen name="Tela 2"
+        component={TabUm}
+        options={{
+          headerShown: false,
+          // title: "Início",
+          tabBarIcon: ({ size, color }) => (
+            <FontAwesome name="globe" color={color} size={size} />
+          ),
+        }} />
+        <Screen name="Tela 3"
+        component={TabUm}
+        options={{
+          headerShown: false,
+          // title: "Início",
+          tabBarIcon: ({ size, color }) => (
+            <FontAwesome name="users" color={color} size={size} />
+          ),
+        }} />
     </Navigator>
-  )
-}
+  );
+};
