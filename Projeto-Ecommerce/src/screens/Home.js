5c0f797dd6087fb80cfa1857aa85a2866e8163dd
digocoutinho78 <digocoutinho@outlook.com>
2024-05-10 15:42:30 -0300

    design-scr-cadastro

--- Projeto-Ecommerce/src/screens/Home.js
+++ Projeto-Ecommerce/src/screens/Home.js
@@ -13,11 +13,22 @@ export default Home = () => {
 
   return(
     <View style={styles.container}>
-      <Text  >Home</Text>
+      <Text style={styles.textoTit} >CADASTRAR PRODUTO</Text>
+      <Text  ></Text>
+      <Text style={styles.texto} >NOME PRODUTO</Text>
       <TextInput style={styles.input} value={nomeProduto} onChangeText={setNomeProduto}/>
-      <TextInput style={styles.input} value={descricao} onChangeText={setDescricao}/>
+      <Text style={styles.texto} >DESCRIÇÃO</Text>
+      <TextInput 
+      // permite a linha infinta
+      multiline 
+      numberOfLines={4}
+        maxLength={120}
+      style={styles.inputDescricao} value={descricao} onChangeText={setDescricao}/>
+      <Text style={styles.texto} >CATEGORIA</Text>
       <TextInput style={styles.input} value={categoria} onChangeText={setCategoria}/>
+      <Text style={styles.texto} >PREÇO</Text>
       <TextInput style={styles.input} value={precoReais} onChangeText={setPrecoReais}/>
+      <Text style={styles.texto} >QUANTIDADE DISPONÍVEL</Text>
       <TextInput  style={styles.input} value={disponibilidade} onChangeText={setDisponibilidade}/>
       <Button title="cadastrar produto" onPress={() => criarProduto({
         nome: nomeProduto,
@@ -39,6 +50,39 @@ const styles = StyleSheet.create({
   },
   input: {
     borderWidth: 2,
-    borderColor: "black"
+    width:250,
+    borderColor: "#A3CEF1",
+    marginTop:10,
+    marginBottom:30,
+    borderRadius: 10,
+    backgroundColor: "#E7ECEF",
+    paddingLeft:10,
+    paddingRight: 10,
+  },
+  inputDescricao: {
+    borderWidth: 2,
+    width:250,
+    borderColor: "#A3CEF1",
+    height: 70,
+    marginTop:10,
+    marginBottom:30,
+    borderRadius: 10,
+    backgroundColor: "#E7ECEF",
+    paddingLeft:10,
+    paddingRight: 10,
+  },
+
+  texto: {
+    color: "#A3CEF1",
+    
+  },
+
+  textoTit: {
+    color: "#E7ECEF",
+    fontSize: 20,
+    fontWeight: "600",
+    marginBottom: 20,
+    marginTop: 20,
+    textAlign: "center"
   }
 });
